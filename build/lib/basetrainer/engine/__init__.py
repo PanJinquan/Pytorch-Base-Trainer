9ed66318fd12c9d914b7a0da094f5e57dc487b58
panjinquan <pan_jinquan@163.com>
2022-01-18 11:12:30 +0800

    update .gitignore

--- build/lib/basetrainer/engine/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*-coding: utf-8 -*-
-"""
-    @Author : panjq
-    @E-mail : pan_jinquan@163.com
-    @Date   : 2021-07-30 19:27:28
-"""
--- build/lib/basetrainer/pruning/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*-coding: utf-8 -*-
-"""
-    @Author : panjq
-    @E-mail : pan_jinquan@163.com
-    @Date   : 2021-12-13 15:13:08
-"""
