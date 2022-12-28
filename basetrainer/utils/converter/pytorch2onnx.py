9f1df11a2e7900a63f39e6dfd04a1fa982fa7b19
潘金全 <panjinquan@dm-ai.com>
2022-12-28 09:50:45 +0800

    update onnx

--- basetrainer/utils/converter/pytorch2onnx.py
+++ basetrainer/utils/converter/pytorch2onnx.py
@@ -11,11 +11,14 @@ import torch.onnx
 import onnx
 
 
-def convert2onnx(model, input_shape, onnx_file="", dynamic=False, simplify=True,
+def convert2onnx(model, input_shape, input_names=['input'], output_names=['output'],
+                 onnx_file="", dynamic=False, simplify=True,
                  use_prune=False, sparsity=0.2, device="cuda:0"):
     """
     :param model: Pytorch 模型
     :param input_shape: 输入维度(B, C, H, W)
+    :param input_names: 输入节点名称
+    :param output_names: 输出节点名称
     :param onnx_file: 输出ONNX模型文件
     :param dynamic:
     :param simplify: 是否对ONNX进行simplify
@@ -43,10 +46,13 @@ def convert2onnx(model, input_shape, onnx_file="", dynamic=False, simplify=True,
     model.eval()
     inputs = torch.randn(1, 3, H, W).to(device)
     do_constant_folding = True
-    torch.onnx.export(model, inputs, onnx_file, verbose=False, export_params=True,
+    torch.onnx.export(model, inputs,
+                      onnx_file,
+                      verbose=False,
+                      export_params=True,
                       do_constant_folding=do_constant_folding,
-                      input_names=['input'],
-                      output_names=['output'])
+                      input_names=input_names,
+                      output_names=output_names)
 
     onnx_model = onnx.load(onnx_file)
     onnx.checker.check_model(onnx_model)
@@ -56,9 +62,7 @@ def convert2onnx(model, input_shape, onnx_file="", dynamic=False, simplify=True,
         try:
             import onnxsim
             print(f'simplifying with onnx-simplifier {onnxsim.__version__}')
-            onnx_model, check = onnxsim.simplify(onnx_model,
-                                                 dynamic_input_shape=dynamic,
-                                                 input_shapes={'images': list(inputs.shape)} if dynamic else None)
+            onnx_model, check = onnxsim.simplify(onnx_model, dynamic_input_shape=dynamic, input_shapes=None)
             onnx.save(onnx_model, onnx_file)
             print("simplifier onnx model:{}".format(onnx_file))
         except Exception as e:
